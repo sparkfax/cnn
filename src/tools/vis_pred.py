73ab14955d3fdb166b79ad3e97a71c6c5c2e775c
sparkfax <chinafax@126.com>
2021-05-17 14:51:54 +0800

    Coco_dataset

--- src/tools/vis_pred.py
+++ src/tools/vis_pred.py
@@ -12,22 +12,37 @@ def _coco_box_to_bbox(box):
   bbox = np.array([box[0], box[1], box[0] + box[2], box[1] + box[3]],
                   dtype=np.int32)
   return bbox
-
+CAT_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+    'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
+    'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
+    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+    'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+    'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+    'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+    'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 _cat_ids = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 
-  14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 
-  24, 25, 27, 28, 31, 32, 33, 34, 35, 36, 
-  37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 
-  48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 
-  58, 59, 60, 61, 62, 63, 64, 65, 67, 70, 
-  72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13,
+  14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+  24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
+  37, 38, 39, 40, 41, 42, 43, 44, 46, 47,
+  48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+  58, 59, 60, 61, 62, 63, 64, 65, 67, 70,
+  72, 73, 74, 75, 76, 77, 78, 79, 80, 81,
   82, 84, 85, 86, 87, 88, 89, 90
 ]
-num_classes = 80
+num_classes = 3
+_valid_ids = [3, 6, 8]
+
 _classes = {
-  ind + 1: cat_id for ind, cat_id in enumerate(_cat_ids)
+  ind + 1: cat_id for ind, cat_id in enumerate(_valid_ids)
 }
-_to_order = {cat_id: ind for ind, cat_id in enumerate(_cat_ids)}
+_to_order = {cat_id: ind for ind, cat_id in enumerate(_valid_ids)}
 coco = coco.COCO(ANN_PATH)
 CAT_NAMES = [coco.loadCats([_classes[i + 1]])[0]['name'] \
               for i in range(num_classes)]
