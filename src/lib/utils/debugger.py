aa07e5091706a10a6d2345e93bd2e99a54e4a25b
sparkfax <chinafax@126.com>
2021-05-17 11:12:27 +0800

    Coco_dataset

--- src/lib/utils/debugger.py
+++ src/lib/utils/debugger.py
@@ -444,21 +444,21 @@ pascal_class_name = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
 
 coco_class_name = ['car', 'bus', 'truck']
 
-coco_class_name = [
-     'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-     'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
-     'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
-     'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
-     'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
-     'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
-     'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
-     'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
-     'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
-     'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
-     'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
-     'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
-     'scissors', 'teddy bear', 'hair drier', 'toothbrush'
-]
+# coco_class_name = [
+#      'person', 'bicycle', 'car', 'motorcycle', 'airplane',
+#      'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
+#      'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+#      'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack',
+#      'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+#      'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+#      'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass',
+#      'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+#      'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+#      'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv',
+#      'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave',
+#      'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase',
+#      'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+# ]
 
 color_list = np.array(
         [
