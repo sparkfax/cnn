c65415affe7eb3f380a40b925ec25c18c1aa1575
sparkfax <chinafax@126.com>
2021-05-20 17:31:31 +0800

    dla basicnet change to CSP

--- src/lib/models/networks/dla_dcn_csp.py
+++ src/lib/models/networks/dla_dcn_csp.py
@@ -61,13 +61,14 @@ class Conv(nn.Module):
 
 class C3(nn.Module):
     # CSP Bottleneck with 3 convolutions
-    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5,stride=1,dilation=1):  # ch_in, ch_out, number, shortcut, groups, expansion
+    def __init__(self, c1, c2, stride=1,dilation=1, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super(C3, self).__init__()
         c_ = int(c2 * e)  # hidden channels
         # self.cv1 = Conv(c1, c_, 1, 1)
         # self.cv2 = Conv(c1, c_, 1, 1)
         self.cv1 = Conv(c1, c_, 1, stride)
         self.cv2 = Conv(c1, c_, 1, stride)
+        n=stride
         self.m = nn.Sequential(*[C3Bottleneck(c_, c_, shortcut, g, e=1.0) for _ in range(n)])
         # self.m = nn.Sequential(*[CrossConv(c_, c_, 3, 1, g, 1.0, shortcut) for _ in range(n)])
         self.cv3 = Conv(2 * c_, c2, 1, stride)  # act=FReLU(c2)
