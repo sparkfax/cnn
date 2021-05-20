b79d5b386b0f2ed1a2e65c6ab82a1a4e48861bbb
sparkfax <chinafax@126.com>
2021-05-20 17:55:19 +0800

    dla basicnet change to CSP

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ def main(opt):
   
   print('Creating model...')
   model = create_model(opt.arch, opt.heads, opt.head_conv)
-  print(model)
+  # print(model)
   optimizer = torch.optim.Adam(model.parameters(), opt.lr)
   start_epoch = 0
   if opt.load_model != '':
