b9445e80abda9429c47be1068f8eb55a718b720b
sparkfax <33086002+sparkfax@users.noreply.github.com>
2021-06-07 15:58:48 +0800

    Update dla_dcn_csp.py

--- src/lib/models/networks/dla_dcn_csp.py
+++ src/lib/models/networks/dla_dcn_csp.py
@@ -83,7 +83,7 @@ class C3(nn.Module):
 #             y+=residual
 
 #             y=self.m(self.cv1(x))
-            print(x.shape, residual.shape)
+            print(x.shape, self.cv2(x).shape, residual.shape)
     
             residual=self.cv2(residual)
             y=self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x),residual), dim=1))
